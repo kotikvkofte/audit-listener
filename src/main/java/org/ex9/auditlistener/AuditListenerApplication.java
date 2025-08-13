99170bfba86735bfb6be107d70c942c95e9963e5
Артём Краковцев <blooender@gmail.com>
2025-08-14 00:03:55 +0700

    chore: init commit

---
+++ src/main/java/org/ex9/auditlistener/AuditListenerApplication.java
@@ -0,0 +1,13 @@
+package org.ex9.auditlistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditListenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditListenerApplication.class, args);
+    }
+
+}
